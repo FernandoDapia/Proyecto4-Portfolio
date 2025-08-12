03ed6f5284ffde6fa399442ebbff9c7bbe7533cf
Fernando Developer <fernandocodigo52@gmail.com>
2025-08-12 23:58:53 +0200

    Primer proyecto colocado en Experience

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -13,8 +13,23 @@ const template = () => {
         <p>Esta es la sección estudios</p>
     </div>
     <div id="section2" class="seccion">
-        <h3>Experiencia</h3>
-        <p>Esta es la sección experiencia</p>
+        <article class="project-1">
+          <div class="project-image">
+            <img src="https://res.cloudinary.com/dy45x7mjl/image/upload/v1755032860/Proyecto-4-Portfolio/Captura_de_pantalla_2025-08-12_a_las_23.05.19_pgvodg.png" alt="Portfolio estático preview" />
+          </div>
+          <div class="project-content">
+            <h3>Proyecto 1 — Landing Page Mercedes-Benz</h3>
+            <p>Proyecto Web Responsive (HTML & CSS) <br>
+            <br>
+- Creación de interfaz adaptable a cualquier dispositivo (Mobile First).<br>
+- Maquetación con Flexbox y CSS Grid.<br>
+- Estilos escalables con variables CSS y reutilización de clases.<br>
+- Enfoque en accesibilidad y experiencia de usuario.<br>
+- Integración de enlaces reales a productos.</p>
+            <p><a href="https://proyecto-1-rockthecode.netlify.app/" target="_blank" rel="noopener">VISITAR</a></p>
+          </div>
+        </article>
+        
     </div>
     
 
